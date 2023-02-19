b49b96688c0dff6ce560c79c41e3e5ab40b070d0
shayanbali <58389567+shayanbali@users.noreply.github.com>
2023-02-19 22:39:22 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import pandas as pd
 
 import numpy as np
